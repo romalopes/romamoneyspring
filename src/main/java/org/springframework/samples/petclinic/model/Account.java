8757db501d6599e35cbc6ea0177916aa1e9b0ed2
romalopes <romalopes@yahoo.com.br>
2013-12-05 22:21:19 +1100

    readme

---
+++ src/main/java/org/springframework/samples/petclinic/model/Account.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Anderson
+ * Date: 05/12/13
+ * Time: 20:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class Account {
+}
---
+++ src/main/java/org/springframework/samples/petclinic/model/Transaction.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Anderson
+ * Date: 05/12/13
+ * Time: 20:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+}
